18fd4625085e9f29a697de6acea666ebfc8cfed8
Andrew <aykim002@gmail.com>
2015-02-22 09:36:08 -0500

    updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Example of lexical scoping using custom functions in R
 
-## Write a short comment describing this function
+## The 'makeCacheMatrix' function intializes an enviroment to return a list of 5 functions + passed matrix parameter
+## The matrix is inverted using the solve() function and the output is stored as the variable 's'
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    s <<- NULL
+  s <- NULL ## initializes value
+  set <- function(y) {  
+    x <<- y  ## operator '<<-' sets y to x which can now be called outside the set function
+    s <<- NULL 
   }
-  get <- function() x
-  setinverse <- function(solve) s <<- solve
-  getinverse <- function() s
-  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+  get <- function() x  ## retrieves value of matrix passed through
+  setinverse <- function(solve) s <<- solve ## calculates and stores the inverse of the passed matrix
+  getinverse <- function() s ## returns the value of s
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse) ## list of functions 
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of a given matrix if it does not exist already
+## Otherwise the function will retrieve the inverse from cache 
 
 cacheSolve <- function(x, ...) {
-  s <- x$getinverse()
-  if(!is.null(s)) {
+  s <- x$getinverse()  ## retrieves s for a given matrix
+  if(!is.null(s)) {  ## if s is not a null value, then the inverse output is cached and can be returned
     message("getting cached data")
     return(s)
   }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setinverse(s)
+  data <- x$get() ## if s is not in cache, then an inverse for it will be computed and cached
+  s <- solve(data, ...) ## computing inverse with the solve function of R
+  x$setinverse(s) ## s is cached 
   s
 }
